226e096ce11c1842cd9517664284a0ecbb04549f
yeteng <1606896936@qq.com>
2019-01-13 11:55:34 +0800

    20190113 python pro changed!

--- Mon2-20190101/正则文件替换/re_test.py
+++ Mon2-20190101/正则文件替换/re_test.py
@@ -1,5 +1,5 @@
 import re
-#
+## 测试字符串 re 替换
 # aaa="ver=10.1.T.T18  aaa"
 # bbb="ver=10.1.T.T18  "
 #
@@ -11,6 +11,7 @@ import re
 # print(r1.groups())
 # print(r2.groups())
 
+## 测试数字填充
 # a='07'
 # b=int(a)+1
 # sb=str(b).zfill(2)
@@ -21,12 +22,16 @@ import re
 # print(l_c)
 # print(c[0:l_c-1])
 
-str1="'C:\\Users\\lenovo\\Desktop\\Test\\\yt\\lm.cfg','Minver ='"
-file1=str1.split(',')[0]
-file2='C:\\Users\\lenovo\\Desktop\\Test\\\yt\\lm.cfg'
-print (file1,file2)
+## 测试读取 windows 路径
+# str1="'C:\\Users\\lenovo\\Desktop\\Test\\\yt\\lm.cfg','Minver ='"
+# file1=str1.split(',')[0]
+# file2='C:\\Users\\lenovo\\Desktop\\Test\\\yt\\lm.cfg'
+# print (file1,file2)
+#
+#
+# with open(file2,'r') as f:
+#     for line in f:
+#         print(line)
 
-
-with open(file2,'r') as f:
-    for line in f:
-        print(line)
+dict1={'name':'AIF333','age':18}
+print('name' in dict1.keys())
--- Mon2-20190101/正则文件替换/版本号自动加1.py
+++ Mon2-20190101/正则文件替换/版本号自动加1.py
@@ -4,11 +4,12 @@
 '''
 import re
 import os
+import json
 
 # 定义版本号自动+1的方法 参数:1.修改文件
 # 2.修改标志 如 ver=   3.加1的位数（如本列，默认是后两位）
 # 4.版本号自动加的位数 默认是 1 eg:ver=10.20.3.T07 => ver=10.20.3.T08
-def verAdd(file,flagstr,changeLength=-2,addValue=1):
+def verAdd(file,flagstr,changeLength=2,addValue=1):
     with open(file,'r') as f1,open("%s.bak" % file,'w') as f2:
         # print("======>",file,flagstr,type(file),type(flagstr))
         for line in f1:
@@ -19,12 +20,16 @@ def verAdd(file,flagstr,changeLength=-2,addValue=1):
             if oldstr:
                 oldstr=oldstr.groups()[0].strip()
                 # 截取后 changeLength=-2 位，如 07 变为整型加后 zfill填充成 08
-                oldstr_ver=oldstr[changeLength:]
+                oldstr_ver=oldstr[-changeLength:]
                 newstr_ver=str(int(oldstr_ver)+addValue).zfill(abs(changeLength))
                 old_num=len(oldstr)
                 newstr=oldstr[:old_num-2]+newstr_ver
+
+                # load json
                 print("文件'%s' 的版本号<%s> ,已自动加%s,新的版本号为<%s>" %
-                      (file,oldstr,addValue,newstr))
+                      (file, oldstr, addValue, newstr))
+                loadDict(file, oldstr, addValue, newstr)
+
                 #写入f2文件
                 f2.write(line.replace(oldstr,newstr))
             else:
@@ -44,13 +49,44 @@ def overFile(oldfile,newfile):
 
 # 通过配置文件实现批量替换,参数：1.文件名  2.分隔符 默认是','
 def delWithCofig(file,split_flag=','):
-    with open(file,'r') as f:
+    with open(file,'r',encoding='utf-8') as f:
         for line in f:
             if not line.startswith("#"):
-                delFile=line.split(split_flag)[0].strip()
-                # 读取配置文件是最后一个会有换行符需剔除
-                delPat=line.split(split_flag)[1].replace('\n','')
-                verAdd(delFile,delPat)
+                if line.strip():
+                    lineList=line.split(split_flag)
+                    lineLen=len(lineList)
+                    # 文件名,正则匹配格式(会有换行符需剔除),自动加的位数,加的值
+                    delFile=lineList[0].strip()
+                    # 读取配置文件是最后一个会有换行符需剔除
+                    delPat=lineList[1].replace('\n','')
 
-pzFile='E:\\Lern\\python\\Lsrc\\Mon2-20190101\\正则文件替换\\pz.txt'
-delWithCofig(pzFile)
+                    if lineLen == 3:
+                        delChangeLength=int(lineList[2].replace('\n',''))
+                        verAdd(delFile, delPat, delChangeLength)
+                    elif lineLen == 4:
+                        delChangeLength =int(lineList[2].replace('\n', ''))
+                        delAddValue=int(lineList[3].replace('\n',''))
+                        verAdd(delFile, delPat, delChangeLength,delAddValue)
+                    else:
+                        verAdd(delFile, delPat)
+
+    with open(jsFile,'w') as f:
+        json.dump(verDict,f)
+
+# 通过json获取版本号字典
+def loadDict(file,oldstr,addValue,newstr):
+    if not file in verDict.keys():
+            verDict[file]=[]
+    verDict[file].append(tuple((oldstr,newstr)))
+
+if __name__ == '__main__':
+    #配置文件 和 版本号序列化文件,用json读出版本字典
+    pzFile='E:\\Lern\\python\\Lsrc\\Mon2-20190101\\正则文件替换\\pz.txt'
+    jsFile='E:\\Lern\\python\\Lsrc\\Mon2-20190101\\正则文件替换\\verHis.json'
+
+    if os.path.exists(jsFile):
+        verDict = json.load(open(jsFile, 'r'))
+    else:
+        verDict={}
+
+    delWithCofig(pzFile)
